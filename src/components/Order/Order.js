409655530293c5bc722d9e3a7b2c0d7184ae0932
Douglas A. Malcolm <douglas@876synergy.com>
2018-08-24 19:55:33 -0500

    printing orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,24 +3,40 @@ import classes from './Order.css';
 
 const order = (props) => {
 
-    
-    let ingredients = Object.keys(props.ingredients).map((igkey) =>{
+    /*
+    let ingredientsOutput = Object.keys(props.ingredients).map((igkey) =>{
         let list = '';
         if(props.ingredients[igkey] !== 0){
             list = <li key={igkey}> {igkey} ({props.ingredients[igkey]})</li>
         }
         return list;
     })
-
-    //console.log(props.ingredients);
+    */
+    const ingredients = [];
+    for(let ingredientName in props.ingredients){
+        ingredients.push(
+            {
+                name: ingredientName,
+                amount: props.ingredients[ingredientName]
+            }
+        );
+    }
+    const ingredientsOutput = ingredients.map(ig =>{
+        return <span 
+                style={{
+                    textTransform: 'capitalize',
+                    display: 'inline-block',
+                    margin: '0 8px',
+                    border: '1px solid #ccc',
+                    padding: '5px'
+                }}
+                key={ig.name}>{ig.name} ({ig.amount}) </span>
+    });
 
     return(
     <div className={classes.Order}>
-        <p>Ingredients:</p>
-        <ul>
-            {ingredients}
-        </ul>
-        <p>Price: <strong>USD ${props.price.toFixed(2)}</strong></p>
+        <p>Ingredients: {ingredientsOutput}</p>
+        <p>Price: <strong>USD ${Number.parseFloat(props.price).toFixed(2)}</strong></p>
     </div>);
 };
 
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,6 +2,7 @@ import React,{ Component } from 'react';
 import Order from '../../components/Order/Order';
 import axios from '../../axios-order';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component{
 
@@ -13,7 +14,14 @@ class Orders extends Component{
     componentDidMount(){
         axios.get('/orders.json')
             .then(response => {
-                this.setState({loading:false,orders:response.data});
+                const fetechOrders = [];
+                for(let key in response.data){
+                    fetechOrders.push({
+                            ...response.data[key],
+                            id: key
+                        });
+                }
+                this.setState({loading:false,orders:fetechOrders});
             })
             .catch(error =>{
                 this.setState({loading:false});
@@ -21,7 +29,12 @@ class Orders extends Component{
     }
 
     render(){
-        
+
+
+        //console.log(this.state.orders);
+
+        let orders = '';
+        /*
         let trans_ingredients = Object.keys(this.state.orders)
         .map(igKey => {
             return (
@@ -41,17 +54,22 @@ class Orders extends Component{
             ingredients={trans_ingredients[igkey]['in']} 
             price={trans_ingredients[igkey]['price']}/>
         });
-
+*/
         if(this.state.loading){
             orders = <Spinner/>;
         }
 
         return(
             <div>
-                {orders}
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id} 
+                        ingredients={order.ingredients}
+                        price={order.price}/> 
+                ))}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders,axios);
